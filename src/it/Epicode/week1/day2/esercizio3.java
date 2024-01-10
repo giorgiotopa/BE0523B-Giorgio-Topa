ccfb9a795e710e650b5c727b6b1b065b8e711a0b
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2024-01-10 09:12:43 +0100

    Esercizio 3

--- src/it/Epicode/week1/day2/esercizio3.java
+++ src/it/Epicode/week1/day2/esercizio3.java
@@ -1,7 +1,29 @@
 package it.Epicode.week1.day2;
 
+import java.util.Scanner;
+
 public class esercizio3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String parola;
 
+        do{
+            System.out.println("Dammi una stringa");
+            parola = scanner.nextLine();
+
+            if(!parola.equals(":q")){
+                for(int i=0; i<parola.length(); i++){
+                    System.out.print(parola.charAt(i));
+
+                    System.out.println();
+                    if(i!=parola.length()-1)
+                        System.out.print(",");
+
+                }
+
+                System.out.println();
+            }
+
+        } while(!parola.equals(":q"));
     }
 }

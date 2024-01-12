8bb116d6c2e173c9d9d681cdcbfcc250ee970c3b
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2024-01-12 16:43:22 +0100

    Week1/Day5/Progetto Settimanale

--- src/it/Epicode/week1/day5_Progetto/LettoreMultimediale.java
+++ src/it/Epicode/week1/day5_Progetto/LettoreMultimediale.java
@@ -9,6 +9,7 @@ public class LettoreMultimediale {
 
         System.out.println("Aggiungi 5 file Multimediali");
 
+        //Ciclo per l'aggiunta degli elementi
         for (int i = 0; i < 5; i++) {
 
             System.out.println("Inserisci il Titolo del file numero: " + (i + 1));
@@ -18,38 +19,39 @@ public class LettoreMultimediale {
             int tipoElemento = scanner.nextInt();
             scanner.nextLine();
 
+            //Switch che controlla la tipologia dell'elemento da inserire e lo istanzia in base alla stessa
             switch (tipoElemento) {
                 case 1:
-                    System.out.println("Volume: (Inserisci un numero intero per assegnare un valore al volume di partenza)");
+                    System.out.println("Volume: (Inserisci un numero intero per assegnare un valore al volume)");
                     int volume = scanner.nextInt();
                     scanner.nextLine();
                     elemento[i] = new Audio(titolo,volume);
                     break;
                 case 2:
-                    System.out.println("Volume: (Inserisci un numero intero per assegnare un valore al volume di partenza)");
+                    System.out.println("Volume: (Inserisci un numero intero per assegnare un valore al volume)");
                     int videoVolume = scanner.nextInt();
-                    System.out.println("Luminosita: (Inserisci un numero intero per assegnare un valore alla luminosità di partenza)");
+                    System.out.println("Luminosita: (Inserisci un numero intero per assegnare un valore alla luminosità)");
                     int luminosita = scanner.nextInt();
                     scanner.nextLine();
                     elemento[i] = new Video(titolo,videoVolume, luminosita);
                     break;
                 case 3:
-                    System.out.println("Luminosita: (Inserisci un numero intero per assegnare un valore alla luminosità di partenza)");
+                    System.out.println("Luminosita: (Inserisci un numero intero per assegnare un valore alla luminosità)");
                     int immagineLuminosita = scanner.nextInt();
                     scanner.nextLine();
                     elemento[i] = new Immagine(titolo, immagineLuminosita);
                     break;
                 default:
-                    System.out.println("Tipo di elemento non valido. Riprova.");
+                    System.out.println("Tipo di file non valido. Riprova.");
                     i--; // Per permettere all'utente di reinserire i dati
                     break;
             }
         }
         int scelta;
         do {
-            System.out.println("Scegli quale elemento eseguire (Inserisci un numero da 1 a 5 per selezionare l'elemento da riprodurre, 0 per terminare): ");
+            System.out.println("Scegli quale file riprodurre (Inserisci un numero da 1 a 5 per selezionare il file, 0 per chiudere il programma): ");
             scelta = scanner.nextInt();
-            scanner.nextLine(); // Consuma il newline residuo dopo nextInt()
+            scanner.nextLine();
 
             if (scelta >= 1 && scelta <= 5) {
                 ElementoMultimediale elementoScelto = elemento[scelta - 1];
@@ -66,7 +68,7 @@ public class LettoreMultimediale {
             }
         } while (scelta != 0);
 
-        System.out.println("Programma terminato.");
+        System.out.println("Programma terminato. Arrivederci!");
         scanner.close();
         }
     }
